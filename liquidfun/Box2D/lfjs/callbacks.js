018d7a963cc43f04cf1fc2740857cd17f8f0a67f
Mark Godwin <m.godwin@orbis.co.uk>
2017-12-20 13:48:17 +0000

    Ability to support multiple worlds in javascript

--- liquidfun/Box2D/lfjs/callbacks.js
+++ liquidfun/Box2D/lfjs/callbacks.js
@@ -1,22 +1,22 @@
 // todo see if switching to function ptrs is faster
 mergeInto(LibraryManager.library, {
-  b2WorldBeginContactBody: function(contactPtr) {
-    b2World.BeginContactBody(contactPtr);
+  b2WorldBeginContactBody: function(worldPtr, contactPtr) {
+    b2World.BeginContactBody(worldPtr, contactPtr);
   },
-  b2WorldEndContactBody: function(contactPtr) {
-    b2World.EndContactBody(contactPtr);
+  b2WorldEndContactBody: function(worldPtr, contactPtr) {
+    b2World.EndContactBody(worldPtr, contactPtr);
   },
-  b2WorldPreSolve: function(contactPtr, oldManifoldPtr) {
-    b2World.PreSolve(contactPtr, oldManifoldPtr);
+  b2WorldPreSolve: function(worldPtr, contactPtr, oldManifoldPtr) {
+    b2World.PreSolve(worldPtr, contactPtr, oldManifoldPtr);
   },
-  b2WorldPostSolve: function(contactPtr, impulsePtr) {
-    b2World.PostSolve(contactPtr, impulsePtr);
+  b2WorldPostSolve: function(worldPtr, contactPtr, impulsePtr) {
+    b2World.PostSolve(worldPtr, contactPtr, impulsePtr);
   },
-  b2WorldQueryAABB: function(fixturePtr) {
-    return b2World.QueryAABB(fixturePtr);
+  b2WorldQueryAABB: function(worldPtr, fixturePtr) {
+    return b2World.QueryAABB(worldPtr, fixturePtr);
   },
-  b2WorldRayCastCallback: function(fixturePtr, pointX, pointY,
+  b2WorldRayCastCallback: function(worldPtr, fixturePtr, pointX, pointY,
                                    normalX, normalY, fraction) {
-    return b2World.RayCast(fixturePtr, pointX, pointY, normalX, normalY, fraction);
+    return b2World.RayCast(worldPtr, fixturePtr, pointX, pointY, normalX, normalY, fraction);
   }
 });
